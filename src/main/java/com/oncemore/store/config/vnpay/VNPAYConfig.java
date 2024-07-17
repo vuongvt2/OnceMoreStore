bd6d667ff5a1b775c3efc8fff8b32bb2ac94e420
Thanh Vuong <vuongvt225@gmail.com>
2024-07-17 23:05:34 +0700

    update timezone

--- src/main/java/com/oncemore/store/config/vnpay/VNPAYConfig.java
+++ src/main/java/com/oncemore/store/config/vnpay/VNPAYConfig.java
@@ -40,7 +40,7 @@ public class VNPAYConfig {
         vnpParamsMap.put("vnp_OrderType", this.orderType);
         vnpParamsMap.put("vnp_Locale", "vn");
         vnpParamsMap.put("vnp_ReturnUrl", this.vnp_ReturnUrl);
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT-7"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnpCreateDate = formatter.format(calendar.getTime());
         vnpParamsMap.put("vnp_CreateDate", vnpCreateDate);
